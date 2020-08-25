eca38be5f2ba32599c9773a036fee68b2ba2e38f
P. L. Lim <2090236+pllim@users.noreply.github.com>
2020-08-25 16:54:46 -0400

    More spacing in pytest header

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
 def pytest_report_header(config):
     import os
 
-    deps = ['Package versions: ']
+    deps = ['', 'Package versions: ']
 
     # Append dependency as needed.
     # https://github.com/PyGithub/PyGithub/issues/1600
     deps.append('PyGitHub: stable')
 
+    deps.append('')
     return os.linesep.join(deps)

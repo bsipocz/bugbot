6861b9b4314629b6ff9d3b8041c51f4f48db1443
P. L. Lim <2090236+pllim@users.noreply.github.com>
2020-08-25 16:18:36 -0400

    A bit less verbose

--- bugbot.py
+++ bugbot.py
@@ -60,7 +60,7 @@ def main(debug=False):
     # Grab code snippet and disable internet.
     raw_code = get_code_snippet(body_content)
     if len(raw_code) == 0:
-        logger.info(f'No code found in {body_content}, stopping')
+        logger.info('No code found, stopping')
         return
     code_lines = inject_no_internet(raw_code)
 
